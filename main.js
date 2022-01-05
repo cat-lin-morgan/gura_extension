7f747d23502a02743f66011c333bdd69479078f4
cat-lin <plummorgan@gmail.com>
2022-01-04 23:52:08 -0800

    Initial commit of Gura browser extension. Contains first icon and logo file.

---
+++ main.js
@@ -0,0 +1,8 @@
+// alert("Yee hawww");
+
+// var images = document.getElementsByTagName('img');
+
+// for (elt of images) {
+//     elt.src = `${chrome.runtime.getURL('aqua.jpg')}`
+//     elt.alt = 'alt text here'
+// }
